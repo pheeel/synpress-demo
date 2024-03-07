50b85e71d05d7f8954f27f1cb3a74bc2ef18db2a
Philipp Panteleev <panteleevphil@gmail.com>
2024-03-07 22:18:05 +0100

    test

--- plywright.config.ts
+++ plywright.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  timeout: 30 * 1000,
+  timeout: 100 * 1000,
   expect: {
-    timeout: 5000,
+    timeout: 40000,
   },
   fullyParallel: true,
   retries: 0,
